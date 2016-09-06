72274a2c1f1c7a58384f0ae62363a75afb5bad2a
Gordon Hall <gordon@gordonwritescode.com>
2016-09-06 18:38:00 -0400

    working on performance tests

--- perf/index.js
+++ perf/index.js
@@ -1,42 +1,81 @@
 'use strict';
 
+var os = require('os');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var rimraf = require('rimraf');
+var utils = require('../lib/utils');
+
 var runWriteSpeedBenchmark = require('./write-speed');
 var runReadSpeedBenchmark = require('./read-speed');
+var runUnlinkSpeedBenchmark = require('./unlink-speed');
 
 /**
  * Runs the performance benchmarks and passes results to callback
  * @param {Object} options
- * @param {String} options.tmpPath - Path to create the temp database
- * @param {Boolean} options.showLogs - Print verbose information to console
+ * @param {String} options.tmpPath - Path to create the sandbox
+ * @param {String} options.tablePath - Path to create the database
  * @param {Function} callback
  */
 module.exports = function(options, callback) {
-  runWriteSpeedBenchmark(options.tmpPath, function(err, writeResults) {
+  runWriteSpeedBenchmark(options, function(err, writeResults) {
     if (err) {
       return callback(err);
     }
 
-    runReadSpeedBenchmark(options.tmpPath, function(err, readResults) {
+    runReadSpeedBenchmark(options, function(err, readResults) {
       if (err) {
         return callback(err);
       }
 
-      callback(null, writeResults, readResults);
+      runUnlinkSpeedBenchmark(options, function(err, unlinkResults) {
+        if (err) {
+          return callback(err);
+        }
+
+        callback(null, writeResults, readResults, unlinkResults);
+      });
     });
   });
 };
 
+/**
+ * Formats the results into a human readable string
+ * @param {Object} writeResults
+ * @param {Object} readResults
+ * @param {Object} unlinkResults
+ */
+module.exports.formatResults = function(writeRes, readRes, unlinkRes) {
+  return {
+    writes: writeRes,
+    reads: readRes,
+    unlinks: unlinkRes
+  };
+};
+
+// NB: If we are running this as a script, go ahead and execute and print out
 if (process.argv[2] === 'exec') {
+  var TMP_PATH = path.join(os.tmpdir(), 'KFS_PERF_SANDBOX');
+  var TABLE_PATH = path.join(TMP_PATH, Date.now().toString());
+
+  if (utils.fileDoesExist(TMP_PATH)) {
+    rimraf.sync(TMP_PATH);
+  }
+
+  mkdirp.sync(TABLE_PATH);
   module.exports({
-    tmpPath: '',
-    showLogs: true
-  }, function(err, writeResults, readResults) {
+    tmpPath: TMP_PATH,
+    tablePath: TABLE_PATH
+  }, function(err, writeResults, readResults, unlinkResults) {
+    console.log('Cleaning test environment...')
+    rimraf.sync(TMP_PATH);
+
     if (err) {
       return console.error('Error running benchmarks:', err);
     }
 
     console.info(
-      module.exports.formatResults(writeResults, readResults)
+      module.exports.formatResults(writeResults, readResults, unlinkResults)
     );
   });
 }
--- perf/write-speed.js
+++ perf/write-speed.js
@@ -1,7 +1,78 @@
 'use strict';
 
 var kfs = require('..');
+var noisegen = require('noisegen');
+var fs = require('fs');
+var async = require('async');
+var path = require('path')
 
 module.exports = function(options, callback) {
+  console.log('Generating some random files, hold on...');
 
+  var results = [];
+  var database = kfs(options.tablePath);
+  var index = 0;
+
+  async.eachSeries([
+    8 * (1024 * 1024),
+    16 * (1024 * 1024),
+    32 * (1024 * 1024),
+    64 * (1024 * 1024),
+    128 * (1024 * 1024),
+    256 * (1024 * 1024),
+  ], function(numBytes, next) {
+    console.log('Preparing %s byte file...', numBytes);
+    var noise = noisegen({ length: numBytes });
+    var file = fs.createWriteStream(
+      path.join(options.tmpPath, index.toString() + '.dat')
+    );
+    index++;
+
+    noise.pipe(file).on('error', next).on('finish', next);
+  }, function(err) {
+    console.log('Test files prepared, writing to KFS...')
+
+    async.eachSeries([
+      '0.dat',
+      '1.dat',
+      '2.dat',
+      '3.dat',
+      '4.dat',
+      '5.dat'
+    ], function(testFileName, next) {
+      var time = 0;
+      var timer = setInterval(function() { time += 10 }, 10);
+      var key = kfs.utils.createReferenceId().toString('hex');
+
+      database.createWriteStream(key, function(err, writeStream) {
+        if (err) {
+          return callback(err);
+        }
+
+        fs.createReadStream(
+          path.join(options.tmpPath, testFileName)
+        ).pipe(writeStream).on('error', function(err) {
+          clearInterval(timer);
+          next(err);
+        }).on('finish', function() {
+          clearInterval(timer);
+          results.push({
+            msElapsed: time,
+            fileKey: key,
+            sBucketIndex: database._getSbucketIndexForKey(key),
+            fileSizeBytes: fs.statSync(
+              path.join(options.tmpPath, testFileName)
+            ).size
+          });
+          next();
+        });
+      });
+    }, function(err) {
+      if (err) {
+        return callback(err);
+      }
+
+      callback(null, results);
+    });
+  });
 };
